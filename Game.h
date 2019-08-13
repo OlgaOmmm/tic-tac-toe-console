258ba7c16165f502f1a327d9404a086905cef00f
Olga <53094440+OlgaOmmm@users.noreply.github.com>
2019-08-13 20:19:59 +0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -2,6 +2,7 @@
 class Game {
 public:
 	Game();
+	void start();
 	void draw();
 	int getPlayerData();
 	void setPlayerData();
@@ -10,7 +11,7 @@ public:
 private:
 	static const int SIZE = 9;
 	char arr[SIZE] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-	int currentPlayer = 1; // or 2
-	char playerSign = 'O'; // or 'X'
-	char inputNumber = ' ';
+	int currentPlayer;
+	char playerSign;
+	char inputNumber;
 };
