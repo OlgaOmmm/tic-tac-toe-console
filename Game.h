53a17e1a9b7f67052fe5c648187beddd2dae2603
Olga <53094440+OlgaOmmm@users.noreply.github.com>
2019-08-19 11:33:24 +0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -5,13 +5,12 @@ public:
 	void start();
 	void draw();
 	char getPlayerData();
-	void setPlayerData(char &inputNumber, char &playerSign);
+	void setPlayerData(char inputNumber, char &playerSign);
 	void playerChange();
 	bool endOfGame();
 private:
 	static const int SIZE = 9;
 	char arr[SIZE] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-	enum players { player1 = 1, player2 } currentPlayer;
+	enum PlayerType { CROSS = 1, NAUGHT } currentPlayer;
 	char playerSign;
-	char inputNumber;
-};
+};	
